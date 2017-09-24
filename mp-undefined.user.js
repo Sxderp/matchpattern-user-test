b078ec3610e161ab797b5381f0e4a3cd98d3eadd
Steven Daniele <sdaniele3@gatech.edu>
2017-09-24 10:03:38 -0500

    user script

---
+++ mp-undefined.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        test
+// @namespace   ncga.me
+// @match       https://*.google.com/foo*bar
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+function gt2(arg1) {
+    return arg1 > 2;
+}
+
+console.log(gt2(1));
+
+
